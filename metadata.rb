79a97cad9380f1e8421f58ba9ef18118bf856a65
Gavin Heavyside <gavin.heavyside@mydrivesolutions.com>
2015-10-22 11:10:18 +0100

    mydrive version number to avoid conflicts

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support+chef@signalfx.com'
 license 'Copyright SignalFx, Inc. All rights reserved'
 description 'This cookbook provides a set of recipes to install and cofigure the collectd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.99'
 
 supports "centos"
 supports "amazon"
